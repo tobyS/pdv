3c1a30d478788e82d8bc4a617ccd98370e487ef8
Tobias Schlitt <toby@php.net>
2015-08-27 15:28:50 +0200

    Added note about failing UltiSnips tests for now.

--- runtests.sh
+++ runtests.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-runVimTests.sh --vimexecutable '/usr/bin/vim' --runtime "bundle/vmustache/autoload/vmustache.vim" --source "`pwd`/autoload/pdv.vim" --source "`pwd`/autoload/parparse.vim" ${1-tests/}
+runVimTests.sh --vimexecutable '/usr/bin/vim' \
+    --runtime "bundle/vmustache/autoload/vmustache.vim" \
+    --source "`pwd`/autoload/pdv.vim" \
+    --source "`pwd`/autoload/parparse.vim" \
+    ${1-tests/}
+
+echo ""
+echo "Note that tests including UltiSnips functionality fail due to missing dependency."
+echo "This will be fixed."
