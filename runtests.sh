ae8ff97e1be6af4b5b6d7cd490b2286614cf043d
Tobias Schlitt <toby@php.net>
2015-08-28 10:19:58 +0200

    Adjusted test runner to VM execution.

--- runtests.sh
+++ runtests.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-runVimTests.sh --vimexecutable '/usr/bin/vim' \
-    --runtime "bundle/vmustache/autoload/vmustache.vim" \
+~/.vim/bundle/runVimTests/bin/runVimTests.sh -2 \
     --source "`pwd`/autoload/pdv.vim" \
     --source "`pwd`/autoload/parparse.vim" \
     ${1-tests/}
-
-echo ""
-echo "Note that tests including UltiSnips functionality fail due to missing dependency."
-echo "This will be fixed."
